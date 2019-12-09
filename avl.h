5e8c35ea9c7f04a9c4ae8134dc39ff3d6b6f346d
reicavera <49381208+reicavera@users.noreply.github.com>
2019-12-08 22:00:48 -0300

    Update avl.h

--- avl.h
+++ avl.h
@@ -1,24 +1,25 @@
 #ifndef AVL
 #define AVL
-#include<stdio.h>
-#include<stdlib.h>
-#include "avl.h"
 
-struct no{
-	//Registro de nó com os campos obrigatórios mais o campo alt,que indica a altura do nó
-	//na avl.  
-	int val,id,op,alt;
-	struct no* pai;
-	struct no* esq;
-	struct no* dir;
-};
 typedef struct no no;
-struct arv{
-	//Registro da avl com os campos raiz e qtd,que indica a quantidade de nós da arvore;
-	no* raiz;
-	int qtd;
-};
 typedef struct arv arv;
+struct no{
+    //Registro de nó com os campos obrigatórios mais o campo alt,que indica a altura do nó
+    //na avl.
+    int val,id,op,alt;
+    struct no* pai;
+    struct no* esq;
+    struct no* dir;
+};
+
+struct arv{
+    //Registro da avl com os campos raiz e qtd,que indica a quantidade de nós da arvore;
+    no* raiz;
+    int qtd;
+};
+
+void destroi_arv(arv* a);//Desaloca a árvore.
+arv* criar_arv();//Aloca e retorna uma árvore.
 void exibir_nivel(arv* a,int i);//Exibe os nós de i-ésimo nível da árvore a.
 no* busca(arv* a,int id);//Retorna o nó de chave id na árvore a ou NULL caso não exista tal nó.
 void remove_no(arv* a,int id);//Remove o nó com chave id.
@@ -28,6 +29,6 @@ void crescente(arv* a);//Exibe os nós com ordem crescente de chave.
 void decrescente(arv* a);//Exibe os nós com ordem decrescente de chave.
 void remove_raiz(arv* a);//Remove e exibe a raiz da árvore. 
 int arv_h(arv* a);//Retorna a altura da árvore.
-int num_clientes(arv* a)//Retorna a quantidade de nós na árvore
+int num_clientes(arv* a);//Retorna a quantidade de nós na árvore
 
-#endif 
+#endif
